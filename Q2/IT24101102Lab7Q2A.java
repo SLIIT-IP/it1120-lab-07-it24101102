4372a821ae6c64192c28f520c68760de88e0fd0b
it24101102 <it24101102@my.sliit.lk>
2024-09-25 18:51:51 +0530

    IT24101102Lab7Q2A

---
+++ Q2/IT24101102Lab7Q2A.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class IT24101102Lab7Q2A{
+  public static void main(String[] args){
+  
+  for(int i = 1; i <= 4; i++){
+    for(int j = 1; j <= 5; j++){
+     System.out.print("$ ");}
+     System.out.println();}
+   }
+ }
+  
